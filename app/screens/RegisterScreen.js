e94f277f5cf2ca390df2cbd12ec6c96c76dfddc8
Amila Perera <amilaperera97@gmail.com>
2022-07-12 22:10:34 +0100

    registration screen - commit 1

---
+++ app/screens/RegisterScreen.js
@@ -0,0 +1,145 @@
+/* eslint-disable prettier/prettier */
+import {useState} from 'react';
+import React from 'react';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
+import {Text} from 'react-native-paper';
+import Background from '../components/Background';
+import Logo from '../components/Logo';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import TextInput from '../components/TextInput';
+import {theme} from '../core/theme';
+import {emailValidator} from '../helpers/emailValidator';
+import {passwordValidator} from '../helpers/passwordValidator';
+import {nameValidator} from '../helpers/nameValidator';
+import {SocialIcon} from 'react-native-elements';
+
+export default function RegisterScreen({navigation}) {
+  const [name, setName] = useState({value: '', error: ''});
+  const [email, setEmail] = useState({value: '', error: ''});
+  const [password, setPassword] = useState({value: '', error: ''});
+
+  const onSignUpPressed = () => {
+    const nameError = nameValidator(name.value);
+    const emailError = emailValidator(email.value);
+    const passwordError = passwordValidator(password.value);
+    if (emailError || passwordError || nameError) {
+      setName({...name, error: nameError});
+      setEmail({...email, error: emailError});
+      setPassword({...password, error: passwordError});
+      return;
+    }
+    navigation.reset({
+      index: 0,
+      routes: [{name: ''}],
+    });
+  };
+
+  return (
+    <Background>
+      <Logo />
+      <Header>Create Account</Header>
+      {/* <Icon name="ios-search" size={20} color="#000"/> */}
+      <TextInput
+        label="First Name"
+        returnKeyType="next"
+        value={name.value}
+        onChangeText={text => setName({value: text, error: ''})}
+        error={!!name.error}
+        errorText={name.error}
+      />
+      <TextInput
+        label="Last Name"
+        returnKeyType="next"
+        value={name.value}
+        onChangeText={text => setName({value: text, error: ''})}
+        error={!!name.error}
+        errorText={name.error}
+      />
+      <TextInput
+        label="Mobile Number"
+        returnKeyType="next"
+        value={name.value}
+        onChangeText={text => setName({value: text, error: ''})}
+        error={!!name.error}
+        errorText={name.error}
+      />
+      <TextInput
+        label="Email"
+        returnKeyType="next"
+        value={email.value}
+        onChangeText={text => setEmail({value: text, error: ''})}
+        error={!!email.error}
+        errorText={email.error}
+        autoCapitalize="none"
+        autoCompleteType="email"
+        textContentType="emailAddress"
+        keyboardType="email-address"
+      />
+      <TextInput
+        label="Password"
+        returnKeyType="done"
+        value={password.value}
+        onChangeText={text => setPassword({value: text, error: ''})}
+        error={!!password.error}
+        errorText={password.error}
+        secureTextEntry
+      />
+      <TextInput
+        label="Confirm Password"
+        returnKeyType="done"
+        value={password.value}
+        onChangeText={text => setPassword({value: text, error: ''})}
+        error={!!password.error}
+        errorText={password.error}
+        secureTextEntry
+      />
+      <Button
+        mode="contained"
+        onPress={onSignUpPressed}
+        style={{marginTop: 24}}>
+        Sign Up
+      </Button>
+      <View style={styles.row}>
+        <Text>or continue with </Text>
+      </View>
+      <View style={styles.row}>
+        <SocialIcon
+          type="facebook"
+          onPress={() => {
+            alert('facebook');
+          }}
+        />
+        <SocialIcon
+          type="google"
+          onPress={() => {
+            alert('google');
+          }}
+        />
+        <SocialIcon
+          type="twitter"
+          onPress={() => {
+            alert('twitter');
+          }}
+        />
+      </View>
+      <View style={styles.row}>
+        <Text>Already have an account? </Text>
+        <TouchableOpacity onPress={() => navigation.replace('RegisterScreen')}>
+          <Text style={styles.link}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </Background>
+  );
+}
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    marginTop: 4,
+  },
+  link: {
+    fontWeight: 'bold',
+    color: theme.colors.primary,
+  },
+});
