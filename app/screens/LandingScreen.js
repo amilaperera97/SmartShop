66a0624b1a8e8f182c6dc4e239d254635af3a29a
Amila Perera <amilaperera97@gmail.com>
2022-08-25 22:39:21 +0100

    Landing screen update

--- app/screens/LandingScreen.js
+++ app/screens/LandingScreen.js
@@ -1,16 +1,22 @@
+/* eslint-disable prettier/prettier */
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import LoginScreen from './LoginScreen';
 import React from 'react';
 import BucketScreen from './BucketScreen';
+import ReceiptsScreen from './ReceiptsScreen';
+import HelpScreen from './HelpScreen';
 
 const Tab = createMaterialBottomTabNavigator();
 
 export default function LandingScreen() {
   return (
-    <Tab.Navigator>
-    <Tab.Screen name="Receipts" component={LoginScreen} />
-    <Tab.Screen name="Bucket" component={BucketScreen} />
-    <Tab.Screen name="Help" component={LoginScreen} />
-  </Tab.Navigator>
+    <Tab.Navigator
+      initialRouteName="Bucket"
+      activeColor="#f0edf6"
+      inactiveColor="#3e2465"
+      barStyle={{ backgroundColor: '#694fad' }}>
+      <Tab.Screen name="Receipts" component={ReceiptsScreen} />
+      <Tab.Screen name="Bucket" component={BucketScreen} />
+      <Tab.Screen name="Help" component={HelpScreen} />
+    </Tab.Navigator>
   );
 }
