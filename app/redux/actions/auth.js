2b09aa343faf95a5d642b3e74b706dd3fbcc1a92
Amila Perera <amilaperera97@gmail.com>
2022-08-25 22:38:39 +0100

    backend service init 2

--- app/redux/actions/auth.js
+++ app/redux/actions/auth.js
@@ -1,11 +1,15 @@
-import AsyncStorage from '@react-native-community/async-storage';
+/* eslint-disable prettier/prettier */
+import AsyncStorage from '@react-native-async-storage/async-storage';
+// import endPoints from '../../api/constants';
 
-// export const SIGNUP = 'SIGNUP';
-// export const LOGIN = 'LOGIN';
+export const SIGNUP = 'SIGNUP';
+export const LOGIN = 'LOGIN';
 export const AUTHENTICATE = 'AUTHENTICATE';
 export const LOGOUT = 'LOGOUT';
 
 let timer;
+
+const base64 = require('base-64');
 
 export const authenticate = (userId, token, expiryTime) => {
   return dispatch => {
@@ -18,66 +22,68 @@ export const authenticate = (userId, token, expiryTime) => {
   };
 };
 
-export const signup = (email, password) => {
+export const signup = (firstName,lastName,username,mobileNumber,password) => {
   return async dispatch => {
+    // const register_uri = endPoints.auth_server_host + endPoints.signup;
     const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD-rRG12jr4nVSAXoh03fYO-GPfOB_1p6I',
+      'http://192.168.166.191:9191/auth-service/api/v1/public/register',
       {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          email,
-          password,
-          returnSecureToken: true,
+          'firstName':firstName.value,
+          'lastName':lastName.value,
+          'username':username.value,
+          'mobileNumber':mobileNumber.value,
+          'password':password.value,
+          // returnSecureToken: true,
         }),
       }
     );
 
-    if (!response.ok) {
+    if (!response.status) {
       const errorResData = await response.json();
-      const errorId = errorResData.error.message;
-      let message = 'Something went wrong!';
-      if (errorId === 'EMAIL_EXISTS') {
-        message = 'This email exists already!';
-      }
+      const message = errorResData.message;
       throw new Error(message);
+    } else {
+      return true;
     }
-
-    const resData = await response.json();
-    dispatch(
-      authenticate(
-        resData.localId,
-        resData.idToken,
-        parseInt(resData.expiresIn) * 1000
-      )
-    );
-    const expirationDate = new Date(
-      new Date().getTime() + parseInt(resData.expiresIn) * 1000
-    );
-    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
+    // const resData = await response.json();
+    // dispatch(
+    //   authenticate(
+    //     resData.localId,
+    //     resData.idToken,
+    //     parseInt(resData.expiresIn) * 1000
+    //   )
+    // );
+    // const expirationDate = new Date(
+    //   new Date().getTime() + parseInt(resData.expiresIn) * 1000
+    // );
+    // saveDataToStorage(resData.idToken, resData.localId, expirationDate);
   };
 };
 
 export const login = (email, password) => {
   return async dispatch => {
     const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD-rRG12jr4nVSAXoh03fYO-GPfOB_1p6I',
+      'http://192.168.166.191:9191/auth-service/oauth/token',
       {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + base64.encode('mobile' + ':' + 'pin'),
         },
-        body: JSON.stringify({
-          email,
-          password,
-          returnSecureToken: true,
-        }),
+        body: new URLSearchParams({
+          'grant_type': 'password',
+          'username': email,
+          'password': password,
+        }).toString(),
       }
     );
 
-    if (!response.ok) {
+    if (!response.status) {
       const errorResData = await response.json();
       const errorId = errorResData.error.message;
       let message = 'Something went wrong!';
@@ -90,19 +96,18 @@ export const login = (email, password) => {
       }
       throw new Error(message);
     }
-
     const resData = await response.json();
     dispatch(
       authenticate(
-        resData.localId,
-        resData.idToken,
-        parseInt(resData.expiresIn) * 1000
+        resData.access_token,
+        resData.refresh_token,
+        parseInt(resData.expires_in) * 1000
       )
     );
     const expirationDate = new Date(
-      new Date().getTime() + parseInt(resData.expiresIn) * 1000
+      new Date().getTime() + parseInt(resData.expires_in) * 1000
     );
-    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
+    saveDataToStorage(resData.access_token, resData.refresh_token, expirationDate);
   };
 };
 
